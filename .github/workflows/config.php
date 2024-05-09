b6bad27ee062419a3e92b339a9a34e79fe17b934
JRivers30 <111069441+JRivers30@users.noreply.github.com>
2024-05-09 01:10:29 -0600

    Add files via upload

--- .github/workflows/config.php
+++ .github/workflows/config.php
@@ -1,5 +1,10 @@
 <?php
-
-$conn = mysqli_connect('localhost','root','','exercisedb');
-
+$host = "localhost";
+$username = "root";  
+$password = "";      
+$database = "phplogin";
+$connection = new mysqli($host, $username, $password, $database);
+if ($connection->connect_error) {
+    die("connection failed: " . $connection->connect_error);
+}
 ?>
